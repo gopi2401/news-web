dda56828923c2f8ef4c0adb9933b222e1f9e3036
gopi2401 <gobinath@streanflo.io>
2024-10-10 20:44:56 +0530

    fix: remove react strict mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import DataProvider from './utils/data';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <DataProvider>
       <App />
     </DataProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
